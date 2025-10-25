59770ca41529a6ea6ff9a590946292ad499f8f4c
Adrian <a.pinzon@correo.unimet.edu.ve>
2025-10-25 09:27:30 -0400

    Cambios en main antes del merge

--- src/proyectoso/Main.java
+++ src/proyectoso/Main.java
@@ -7,11 +7,6 @@ package proyectoso;
 import proyectoso.controlador.ControladorSimulador;
 import proyectoso.vista.VentanaPrincipal;
 
-/*
- * Se encarga de inicializar el patrón de diseño MVC (Modelo-Vista-Controlador)
- * para el Simulador de Planificación de Procesos y de manejar cualquier
- * error crítico durante la carga inicial.
-*/
 
 public class Main {
     public static void main(String[] args) {
