37a670ac7a73212e54ca398c460703beedb3cb30
VirginiaSTR <71676092+VirginiaSTR@users.noreply.github.com>
2025-10-24 20:05:34 -0400

    Cambios en Planificador

--- src/proyectoso/modelo/Planificador.java
+++ src/proyectoso/modelo/Planificador.java
@@ -4,6 +4,12 @@
  */
 package proyectoso.modelo;
 
+/*
+ * Interfaz de Planificación
+ * permite cambiar dinámicamente el algoritmo 
+ * de planificación (FCFS, HRRN, Feedback, etc.) sin cambiar su lógica interna.
+*/
+
 public interface Planificador {
     // AÑADIR el ciclo actual (necesario para HRRN)
     PCB seleccionarSiguiente(ColaPCB colaListos, int cicloActual); // 👈 CAMBIO AQUÍ
